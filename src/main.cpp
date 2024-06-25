e9c5ff1e03118456be7bf8408de3c2465d754062
redflameken2 <202301178@iacademy.edu.ph>
2024-06-25 19:56:58 +0800

    Studied threads, update only works on linux

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,40 @@
+#include <cstdlib>
 #include <iostream>
+#include <thread>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::thread;
+using std::this_thread::sleep_for;
+using std::system;
+
+void notify(){
+    using namespace std::chrono_literals;
+    while(true){
+        system("notify-send -u normal 'bruh look at me'");
+        sleep_for(5s);
+    }
+}
+
+void f2(){
+    while(true){
+        string fuck;
+        cout << "Enter anything: ";
+        cin >> fuck;
+        cout << "Output:\n" << fuck << endl;
+    }
+}
+
+void initThread() {
+    thread t1(notify);
+    thread t2(f2);
+    t1.join();
+    t2.join();
+}
 
 int main(){
-    cout << "Bruh what";
+    initThread();
     return 0;
 }
