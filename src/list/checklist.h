c8bcb91a2c28a9af55147043189584584d4e3f6e
redflameken2 <202301178@iacademy.edu.ph>
2024-07-13 17:27:16 +0800

    Moved CheckList and CheckItem to their own header file (src/list/checklist.h)

---
+++ src/list/checklist.h
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using std::string;
+using std::vector;
+using std::cout;
+
+struct CheckItem {
+    string title;
+    bool checked = false;
+    CheckItem(string title, bool checked) : title(title), checked(checked){
+    }
+    CheckItem(){};
+};
+class CheckList {
+public:
+    vector<CheckItem> checkItems;
+    void add(string title, bool checked){
+        checkItems.push_back(CheckItem(title, checked));
+    }
+    void add(CheckItem checkItem){
+        checkItems.push_back(checkItem);
+    }
+    bool isChecked(int index){
+        return checkItems[index].checked;
+    }
+    string getTitle(int index){
+        return checkItems[index].title;
+    }
+    int size(){
+        return checkItems.size();
+    }
+    string getDBFormat(){
+        string write;
+        write += "{";
+        for(CheckItem checkItem : checkItems){
+            write+= "[";
+            write+= checkItem.title;
+            write+= ",";
+            if(checkItem.checked)
+                write+= "true";
+            else
+                write+= "false";
+            write+= "]";
+        }
+        write += "}";
+        return write;
+    }
+    void printCheckList(){
+        if(checkItems.empty()){
+            cout << "There is no CheckList!\n";
+            return;
+        }
+        cout << "CheckList: \n";
+        for(CheckItem checkItem : checkItems)
+            cout << "[" << (checkItem.checked ? "x" : " ") << "] " << checkItem.title << "\n";
+    }
+};
+
--- src/list/notes.h
+++ src/list/notes.h
@@ -8,6 +8,7 @@
 #include <fstream>
 #include "../util/helpers.h"
 #include "duedate.h"
+#include "checklist.h"
 
 using std::cout;
 using std::cin;
@@ -19,58 +20,6 @@ using std::vector;
 using std::ofstream;
 using std::ifstream;
 using std::to_string;
-
-struct CheckItem {
-    string title;
-    bool checked = false;
-    CheckItem(string title, bool checked) : title(title), checked(checked){
-    }
-    CheckItem(){};
-};
-class CheckList {
-public:
-    vector<CheckItem> checkItems;
-    void add(string title, bool checked){
-        checkItems.push_back(CheckItem(title, checked));
-    }
-    void add(CheckItem checkItem){
-        checkItems.push_back(checkItem);
-    }
-    bool isChecked(int index){
-        return checkItems[index].checked;
-    }
-    string getTitle(int index){
-        return checkItems[index].title;
-    }
-    int size(){
-        return checkItems.size();
-    }
-    string getDBFormat(){
-        string write;
-        write += "{";
-        for(CheckItem checkItem : checkItems){
-            write+= "[";
-            write+= checkItem.title;
-            write+= ",";
-            if(checkItem.checked)
-                write+= "true";
-            else
-                write+= "false";
-            write+= "]";
-        }
-        write += "}";
-        return write;
-    }
-    void printCheckList(){
-        if(checkItems.empty()){
-            cout << "There is no CheckList!\n";
-            return;
-        }
-        cout << "CheckList: \n";
-        for(CheckItem checkItem : checkItems)
-            cout << "[" << (checkItem.checked ? "x" : " ") << "] " << checkItem.title << "\n";
-    }
-};
 
 class Note {
 private:
@@ -124,10 +73,3 @@ public:
         this->dueDate = note.dueDate;
     }
 };
-
-//Functions
-/*string * getDataArray(string data);*/
-Note getNoteFromDB();
-string * getDataArray(string data);
-vector<CheckItem> getChecklistData(string data);
-Note getNoteFromDB();
